b23c26ed15c3ef9516706b3ac0a2c93a8e3bfca0
Caleb Atwood <calebatwood@Calebs-MacBook-Air.local>
2015-02-10 14:55:07 -0700

    create company show page

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,5 +3,10 @@ class CompaniesController < ApplicationController
   def index
     @companies = Company.all
   end
-  
+
+  def show
+    @company = Company.find(params[:id])
+    @locations = @company.locations
+    @products = @company.products
+  end
 end
